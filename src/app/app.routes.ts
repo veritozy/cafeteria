b0b835ad5083197326337a10d6af6e06f555f933
veritozy <verozy26@gmail.com>
2026-07-03 15:11:31 -0500

    usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Nosotros } from './features/nosotros/nosotros';
 import { Home } from './features/home/home';
+import { Usuarios } from './features/usuarios/usuarios';
 
 export const routes: Routes = [
     {path:"home", component:Home},
-    {path:"nosotros", component:Nosotros}
+    {path:"nosotros", component:Nosotros},
+    {path:"usuarios", component:Usuarios}
 
 ];
